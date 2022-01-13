9a44126965d9735733c435ff8d7ee69a7e18bb89
904678748@qq.com <904678748@qq.com>
2022-01-13 10:18:52 +0800

    ..

--- src/router/index.js
+++ src/router/index.js
@@ -23,22 +23,33 @@ export default new Router({
       children:adminRouter.router,
     },
     {
-      path: '*',
+      path: '/404',
       name: '/404',
       component: r => require.ensure([], () => r(require('@/components/404')), 'index'),
+    },
+    {//重定向中间件
+      path: '*',
+      name: 'reset',
       beforeEnter: (to, from, next) => {
         let originUrl = localStorage.getItem('COM+');
         localStorage.removeItem('COM+');
-        if (originUrl == null) { next('/404'); return; }
+        if (originUrl == null) {
+          next('/404');
+          return;
+        }
         let ticketRegex = /\?ticket=([^#]+)#/;
 
         let regexResult = ticketRegex.exec(location.href);
         if (regexResult.length > 1) {
           let ticket = regexResult[1];
-          let ticketHref = `http://192.168.21.43:8021/api/third-part-auth/cas-proxy?ticket=${ticket}&service=${encodeURIComponent(location.origin)}`;
+          let ticketHref = `${process.env.VUE_APP_BASE_API}useridentify/api/third-part-auth/cas-proxy?ticket=${ticket}&service=${encodeURIComponent(originUrl)}`;
           axios({
             url: ticketHref,
-            method: 'get'
+            method: 'get',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization':'Bearer ' + localStorage.getItem('BasicToken')
+            },
           })
             .then(x => {
 
