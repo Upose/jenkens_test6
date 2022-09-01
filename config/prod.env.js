a3eb2343fd28087faa941e666f92aee3b9c2c335
gongqin <gongqin@vipinfo.com.cn>
2022-09-01 16:16:09 +0800

    修改配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,12 +3,12 @@
  * @Author: gongqin
  * @Date: 2022-03-18 10:36:32
  * @LastEditors: gongqin
- * @LastEditTime: 2022-09-01 15:47:30
+ * @LastEditTime: 2022-09-01 16:13:18
  */
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_FILE_URL: './cdn/'
+  VUE_APP_FILE_URL: '../cdn/'
   // VUE_APP_IMG_URL:'"http://192.168.21.46:6900/"',//图片路径、域名前缀
   // VUE_APP_template_url:'"http://192.168.21.71:9000"',//模板路径名
   // VUE_APP_BASE_API:'"http://192.168.21.46:8000"',//请求地址
