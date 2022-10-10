31916a470e7f9732573366e53823b8232aa4018e
qhx0807 <iqx7@foxmail.com>
2022-10-10 13:50:09 +0800

    富文本新增文件上传功能

--- src/components/admin/information/add_news.vue
+++ src/components/admin/information/add_news.vue
@@ -221,25 +221,20 @@ export default {
       language: 'zh_CN',
       height: 400,
       min_height: 400,
-      // plugins: 'code, image, autoresize, lists',
-      // toolbar: 'code image',
+      width: 836,
       toolbar_mode : 'wrap',
       branding: false,
       // toolbar_sticky: true,
       resize: true,
       fontsize_formats: '11px 12px 14px 16px 18px 24px 36px 48px',
       font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
-      // plugins: 'image,wordcount,charmap,code,hr,lists,advlist,emoticons,fullscreen,help,insertdatetime,link',
-
-      plugins: 'code  quickbars print preview searchreplace autolink directionality powerpaste visualblocks visualchars image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount textpattern help emoticons autosave bdmap indent2em autoresize kityformula-editor',
+      plugins: 'code upfile quickbars print preview searchreplace autolink directionality powerpaste visualblocks visualchars image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount textpattern help emoticons autosave bdmap indent2em autoresize kityformula-editor',
       toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor codesample | alignleft aligncenter alignright alignjustify outdent indent | \
           styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
-          table image  media charmap emoticons hr pagebreak insertdatetime print preview | bdmap indent2em lineheight kityformula-editor',
+          table image media upfile charmap emoticons hr pagebreak insertdatetime print preview bdmap indent2em lineheight kityformula-editor',
       images_upload_handler: (blobInfo, success, failure) => { // 图片上传
-        console.log(blobInfo, success, failure);
         this.handleImgUpload(blobInfo, success, failure)
       },
-      width: 834,
       powerpaste_word_import: "merge",
       powerpaste_html_import: 'merge',
       powerpaste_allow_local_images: true,
@@ -248,6 +243,7 @@ export default {
       file_picker_types: 'media',
       media_live_embeds: true,
       file_picker_callback: (callback, value, meta) => {
+        console.log(meta);
         let input = document.createElement('input');
           input.setAttribute('type', 'file');
           input.setAttribute("accept", ".mp4");
@@ -261,8 +257,10 @@ export default {
           input.click();
       },
       video_template_callback: data => {
-        console.log(data)
         return '<span class="mce-preview-object mce-object-video" contenteditable="false" data-mce-object="video" data-mce-p-allowfullscreen="allowfullscreen" data-mce-p-frameborder="no" data-mce-p-scrolling="no" data-mce-p-src='+data.source+' data-mce-p-width='+data.width+' data-mce-p-height='+data.height+' data-mce-p-controls="controls" data-mce-html="%20"> <video width='+data.width+' height='+data.height+' controls="controls"> <source src='+data.source+' type='+data.sourcemime+'></source> </video> </span>';
+      },
+      file_callback: (file, succFun) => {
+        this.handleFileUpload(file, succFun);
       }
     });
     tinyMCE.activeEditor.on('paste', function(e) {
@@ -470,6 +468,13 @@ export default {
     handleVideoUpload(fd, callback) {
       this.http.postFile('', fd).then(res => {
         callback(this.fileUrl+res.data)
+      }).then(err => {});
+    },
+    handleFileUpload(file, succFun) {
+      const formData =  new FormData();
+      formData.append('files', file);
+      this.http.postFile('', formData).then(res => {
+        succFun(this.fileUrl+res.data[0], {text: file.name});
       }).then(err => {});
     },
     //标签选择
