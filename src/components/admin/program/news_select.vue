4123e9bd99513c450adeb83cf34e2ad7551c52f5
gongqin <gongqin@vipinfo.com.cn>
2022-12-07 11:15:25 +0800

    清空

--- src/components/admin/program/news_select.vue
+++ src/components/admin/program/news_select.vue
@@ -2,57 +2,63 @@
 <template>
   <div class="admin-warp-page">
     <el-container>
-      <el-aside width="auto" :collapse="$root.collapse" :class="$root.collapse?'fold-menu':''"><serviceLMenu :isActive="1"></serviceLMenu></el-aside>
-      <el-main class="admin-content pd admin-bg-top" :class="{'content-collapse':$root.collapse}">
+      <el-aside width="auto" :collapse="$root.collapse" :class="$root.collapse ? 'fold-menu' : ''">
+        <serviceLMenu :isActive="1"></serviceLMenu>
+      </el-aside>
+      <el-main class="admin-content pd admin-bg-top" :class="{ 'content-collapse': $root.collapse }">
         <breadcrumb :cuMenu="'新闻发布'" :fontColor="'fff'"></breadcrumb><!--面包屑导航--->
         <div class="content search-table-general">
-            <div class="search-table-w">
-                <h1 class="search-title">新闻检索</h1>
-                <div class="search-term">
-                    <el-input class="width187" v-model="postForm.searchKey" size="medium" placeholder="新闻全文检索"></el-input><!--上一个页面中的检索条件要塞入此输入框中-->
-                    <el-button type="primary" size="medium" icon="el-icon-search" @click="searchClick()">查找</el-button>
-                </div>
-            </div><!--顶部查询 end-->
-            <div class="table-w">
-              <h2 class="m-title"><i class="el-icon-date"></i>检索结果列表<div class="r-btn">
-                    <!-- <el-button type="primary" size="medium" class="blue-btn">新增管理员</el-button> -->
-                </div></h2>
-                <div class="t-p">
-                    <el-table stripe :data="tableData" @selection-change="handleSelectionApp" border class="admin-table">
-                        <!-- <el-table-column type="selection" width="45"></el-table-column> -->
-                        <el-table-column type="index" width="58" align="center" label="序号">
-                          <template slot-scope="scope">
-                            {{getTableSort(scope.$index)}}
-                          </template>
-                        </el-table-column>
-                        <el-table-column prop="title" label="标题">
-                          <template slot-scope="scope">
-                            <span class="news-title" :title="scope.row.title" @click="handleEdit(scope.row)">{{scope.row.title}}</span>
-                          </template>
-                        </el-table-column>
-                        <el-table-column prop="columnIDs" label="所属栏目">
-                           <template slot-scope="scope">
-                             <span class="news-title" v-for="(item,index) in scope.row.columnIDs"  @click="ColumnEdit(item)" :key="index+'_'+item.title">{{item.value}};</span>
-                           </template>
-                        </el-table-column>
-                        <el-table-column prop="publisher" align="center" label="发布者" width="110"></el-table-column>
-                        <el-table-column align="center" prop="publishDate" label="发布时间" width="100">
-                          <template slot-scope="scope">
-                             <span>{{(scope.row.publishDate||'0000-00-00').slice(0,10)}}</span>
-                           </template>
-                        </el-table-column>
-                        <el-table-column align="center" prop="updateTime" label="更新时间" width="100">
-                          <template slot-scope="scope">
-                             <span>{{(scope.row.updateTime||'0000-00-00').slice(0,10)}}</span>
-                           </template>
-                        </el-table-column>
-                    </el-table>
-                    <!--<el-button size="small" class="default-btn-n-border next-btn">下一页</el-button>-->
-                    <paging :pagedata="pageData" @pagechange="pageChange" v-if="pageData.totalCount"></paging>
+          <div class="search-table-w">
+            <h1 class="search-title">新闻检索</h1>
+            <div class="search-term">
+              <el-input class="width187" v-model="postForm.searchKey" size="medium" placeholder="新闻全文检索"
+                clearable></el-input><!--上一个页面中的检索条件要塞入此输入框中-->
+              <el-button type="primary" size="medium" icon="el-icon-search" @click="searchClick()">查找</el-button>
+            </div>
+          </div><!--顶部查询 end-->
+          <div class="table-w">
+            <h2 class="m-title"><i class="el-icon-date"></i>检索结果列表<div class="r-btn">
+                <!-- <el-button type="primary" size="medium" class="blue-btn">新增管理员</el-button> -->
+              </div>
+            </h2>
+            <div class="t-p">
+              <el-table stripe :data="tableData" @selection-change="handleSelectionApp" border class="admin-table">
+                <!-- <el-table-column type="selection" width="45"></el-table-column> -->
+                <el-table-column type="index" width="58" align="center" label="序号">
+                  <template slot-scope="scope">
+                    {{ getTableSort(scope.$index) }}
+                  </template>
+                </el-table-column>
+                <el-table-column prop="title" label="标题">
+                  <template slot-scope="scope">
+                    <span class="news-title" :title="scope.row.title"
+                      @click="handleEdit(scope.row)">{{ scope.row.title }}</span>
+                  </template>
+                </el-table-column>
+                <el-table-column prop="columnIDs" label="所属栏目">
+                  <template slot-scope="scope">
+                    <span class="news-title" v-for="(item, index) in scope.row.columnIDs" @click="ColumnEdit(item)"
+                      :key="index + '_' + item.title">{{ item.value }};</span>
+                  </template>
+                </el-table-column>
+                <el-table-column prop="publisher" align="center" label="发布者" width="110"></el-table-column>
+                <el-table-column align="center" prop="publishDate" label="发布时间" width="100">
+                  <template slot-scope="scope">
+                    <span>{{ (scope.row.publishDate || '0000-00-00').slice(0, 10) }}</span>
+                  </template>
+                </el-table-column>
+                <el-table-column align="center" prop="updateTime" label="更新时间" width="100">
+                  <template slot-scope="scope">
+                    <span>{{ (scope.row.updateTime || '0000-00-00').slice(0, 10) }}</span>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <!--<el-button size="small" class="default-btn-n-border next-btn">下一页</el-button>-->
+              <paging :pagedata="pageData" @pagechange="pageChange" v-if="pageData.totalCount"></paging>
 
-                </div>
-            </div><!--管理页列表 end--->
-            
+            </div>
+          </div><!--管理页列表 end--->
+
         </div><!---顶部查询板块 end--->
         <footerPage class="top20"></footerPage>
       </el-main>
@@ -60,7 +66,7 @@
     <el-dialog append-to-body title="备注信息" :visible.sync="show_remark" width="550px" :close-on-click-modal="false">
       <p>部分功能存在违规行为，请修改后再提交!</p>
       <span slot="footer" class="dialog-footer">
-          <el-button icon="iconfont el-icon-vip-quxiao" size="medium" @click="show_remark=false">关闭</el-button>
+        <el-button icon="iconfont el-icon-vip-quxiao" size="medium" @click="show_remark = false">关闭</el-button>
       </span>
     </el-dialog>
   </div>
@@ -73,57 +79,57 @@ import serviceLMenu from "@/components/admin/common/serviceLMenu";
 import paging from "@/components/admin/common/paging";
 export default {
   name: 'index',
-  created(){
+  created() {
     this.bus.$on('collapse', msg => {
       this.$root.collapse = msg;
     })
   },
-  components:{footerPage,serviceLMenu,breadcrumb,paging},
-  data () {
+  components: { footerPage, serviceLMenu, breadcrumb, paging },
+  data() {
     return {
       pageData: {
         pageIndex: 1,
         pageSize: 50,
       },//分页参数
-      postForm:{
+      postForm: {
         "pageIndex": 1,
         "pageSize": 50,
-        "searchKey": decodeURI(this.$route.query.title||''),
+        "searchKey": decodeURI(this.$route.query.title || ''),
       },
-      tableData:[],
+      tableData: [],
     }
   },
-  mounted(){
-      this.initData();
+  mounted() {
+    this.initData();
   },
-  methods:{
+  methods: {
     getTableSort(index) {
-      return (index+1)+(this.pageData.pageSize*(this.pageData.pageIndex-1))
+      return (index + 1) + (this.pageData.pageSize * (this.pageData.pageIndex - 1))
     },
     //编辑新闻
-    handleEdit(row){
+    handleEdit(row) {
       console.log(row.columnIDs[0]);
-      this.$router.push({path:'/admin_addNews',query:{id:row.id,c_id:row.columnIDs[0].key}})
+      this.$router.push({ path: '/admin_addNews', query: { id: row.id, c_id: row.columnIDs[0].key } })
     },
     //编辑栏目
-    ColumnEdit(row){
+    ColumnEdit(row) {
       // this.$router.push({path:'/admin_programAdd',query:{id:row.key}})
-      this.$router.push({path:'/admin_programInfo',query:{id:row.key}})
+      this.$router.push({ path: '/admin_programInfo', query: { id: row.key } })
     },
-    initData(){
+    initData() {
       this.initpageData();
       this.postForm.pageIndex = this.pageData.pageIndex;
       this.postForm.pageSize = this.pageData.pageSize;
-      this.http.postJson('news-content-get-by-search',this.postForm).then(res=>{
-        this.tableData = res.data.items||[];
+      this.http.postJson('news-content-get-by-search', this.postForm).then(res => {
+        this.tableData = res.data.items || [];
         this.pageData.totalCount = res.data.totalCount;
-      }).catch(err=>{
-          console.log(err);
+      }).catch(err => {
+        console.log(err);
       })
     },
-    initpageData(){
+    initpageData() {
       this.pageData = {
-        pageIndex:this.pageData.pageIndex,
+        pageIndex: this.pageData.pageIndex,
         pageSize: this.pageData.pageSize,
       };
     },
@@ -133,13 +139,13 @@ export default {
       this.initData();
     },
     //查询
-    searchClick(){
+    searchClick() {
       this.pageData.pageIndex = 1;
       this.initData();
     },
     /**应用表格选择框**/
     handleSelectionApp(val) {
-        this.multipleSelection = val;
+      this.multipleSelection = val;
     },
   },
 }
@@ -149,13 +155,15 @@ export default {
 @import "../../../assets/admin/css/color.less";
 @import "../../../assets/admin/css/style.less";
 @import "../../../assets/admin/css/table.less";
-.news-title{
+
+.news-title {
   overflow: hidden;
-  text-overflow:ellipsis;
+  text-overflow: ellipsis;
   white-space: nowrap;
   color: @m-col-b7;
   cursor: pointer;
-  &:hover{
+
+  &:hover {
     border-bottom: 1px solid @m-col-b7;
   }
 }
