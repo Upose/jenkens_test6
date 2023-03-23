126722b2febfd7eccfc1ecd3b2628cf105d77c70
LuJiangPeng-lxx <lujiangpeng@vipinfo.com.cn>
2023-03-23 09:35:39 +0800

    调整新闻读者评论加载顺序

--- src/components/web/view/news_details.vue
+++ src/components/web/view/news_details.vue
@@ -2,7 +2,7 @@
  * @Author: lxx1997 lujiangpeng@vipinfo.com.cn
  * @Date: 2023-03-14 10:31:29
  * @LastEditors: LuJiangPeng-lxx lujiangpeng@vipinfo.com.cn
- * @LastEditTime: 2023-03-22 21:59:37
+ * @LastEditTime: 2023-03-23 09:32:03
  * @FilePath: \news_sys\src\components\web\view\news_details.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -93,7 +93,7 @@ export default {
     this.initData();
   },
   mounted() {
-    this.addComponent();
+    // this.addComponent();
   },
   methods: {
     detailsData(data) {
@@ -101,6 +101,7 @@ export default {
       this.title = data.title;
       this.objectid = data.id;
       this.columnid = data.columnID;
+      this.addComponent();
     },
     addComponent() {
       setTimeout(() => {
